a29cbf4d9e61a38d86c33f3419abcfeefa1a3ca5
zoyoy1203 <2071686148@qq.com>
2019-10-13 01:48:38 +0800

    test 回调测试

---
+++ test/stage.js
@@ -0,0 +1,63 @@
+const { readFile } = require('fs')
+const EventEmitter = require('events')
+
+class EE extends EventEmitter {}
+
+const yy = new EE()
+
+yy.on('evnet', () => {
+    console.log('出大事了！')
+})
+
+setTimeout(() => {
+    console.log('0 毫秒后到期执行的定时器回调')
+},0)
+
+setTimeout(() => {
+    console.log('100 毫秒后到期执行的定时器回调')
+},100)
+
+setTimeout(() => {
+    console.log('200 毫秒后到期执行的定时器回调')
+},200)
+
+readFile('../package.json','utf-8', data => {
+    console.log('完成文件 1 读操作的回调')
+})
+
+readFile('../README.md','utf-8', data => {
+    console.log('完成文件 2 读操作的回调')
+})
+
+setImmediate(() => {
+    console.log('immediate 立即回调')
+})
+
+process.nextTick(() => {
+    console.log('process.nextTick 的回调')
+})
+
+Promise.resolve()
+    .then(() => {
+        yy.emit('evnet')
+
+        process.nextTick(() => {
+            console.log('process.nextTick的第2次回调')
+        })
+        console.log('Promise 的第 1 次回调')
+    })
+    .then(() => {
+        console.log('Promise 的第2次回调')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
