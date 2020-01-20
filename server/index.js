4a2e7962d656bc945458e1fe7f89ffe1272cde70
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-01-20 16:46:35 +0800

    DPlayer里的screenshot参数需要设置跨域，这里把它去掉

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const Koa = require('koa')
-const mongoose = require('mongoose')
+const app = new Koa()
+
+const cors = require('koa2-cors');
+// 处理跨域
+app.use(cors())
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    await next();
+});
+
+
+mongoose = require('mongoose')
 const views = require('koa-views')
 const { resolve } = require('path')
 const { connect, initSchemas } = require('./database/init')
-
 
 // ;(async () => {
 //     await connect()
@@ -14,21 +24,19 @@ const { connect, initSchemas } = require('./database/init')
 //     // console.log(movies)
 // })()
 
-const app = new Koa()
-
-// 处理跨域
-
 
 app.use(views(resolve(__dirname, './views'), {
     extension: 'pug'   // 只要后缀名为pug的文件，都会被识别为模板文件
 }))
 
 app.use(async (ctx, next) => {
-   await ctx.render('index', {
-       you: 'Luke',
-       me: 'Scott'
-   })
+   
+    await ctx.render('index', {
+        you: 'Luke',
+        me: 'Scott'
+    })
 })
+
 
 
 
